f320b9aec716b0bae029efe378c3f5b662690926
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2024-12-29 00:51:49 +0530

    add user controller

---
+++ sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.sweat_net.sweatNet_backend.controller;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sweat_net.sweatNet_backend.dto.request.UserRequest;
+import com.sweat_net.sweatNet_backend.dto.response.UserMResponse;
+import com.sweat_net.sweatNet_backend.dto.response.UserResponse;
+import com.sweat_net.sweatNet_backend.service.UserServise;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserServise userServise;
+
+    @PostMapping("/register")
+    public UserMResponse register(@RequestBody UserRequest userRequest) {
+        return userServise.register(userRequest);
+    }
+
+    @GetMapping("/{id}/profile")
+    public UserResponse getUserById(@PathVariable String id) {
+        return userServise.getUserById(id);
+    }
+
+    @PutMapping("/{id}/profile")
+    public UserMResponse updateUser(@PathVariable String id, @RequestBody UserRequest userRequest) {
+        return userServise.updateUser(id, userRequest);
+    }
+
+    @DeleteMapping("{id}/profile")
+    public UserMResponse deleteUser(@PathVariable String id) {
+        return userServise.deleteUser(id);
+    }
+
+}
