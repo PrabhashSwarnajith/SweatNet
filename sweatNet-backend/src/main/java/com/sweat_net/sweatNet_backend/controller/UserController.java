04fe88cfe473e03aedc33502677fa7f6232dd580
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2024-12-29 01:10:42 +0530

    remove unused import for Optional in UserController

--- sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/controller/UserController.java
+++ sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.sweat_net.sweatNet_backend.controller;
 
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
