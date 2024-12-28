067786afa84cb959e87b45bb9d4facb2cea7c4c0
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2024-12-29 00:57:33 +0530

    add user repository methods for username and email lookup

--- sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/repository/UserRepository.java
+++ sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/repository/UserRepository.java
@@ -1,9 +1,21 @@
 package com.sweat_net.sweatNet_backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sweat_net.sweatNet_backend.model.User;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    User findByUsernameOrEmail(String username, String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 
 }
