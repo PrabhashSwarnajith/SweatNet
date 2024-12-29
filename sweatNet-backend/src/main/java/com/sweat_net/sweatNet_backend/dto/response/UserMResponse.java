430a4d324e40a2b77d2b4b14c877f1c13a2bf2f8
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2024-12-29 11:48:28 +0530

    feat: add JsonInclude annotation to UserMResponse for null value handling

--- sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/dto/response/UserMResponse.java
+++ sweatNet-backend/src/main/java/com/sweat_net/sweatNet_backend/dto/response/UserMResponse.java
@@ -1,11 +1,14 @@
 package com.sweat_net.sweatNet_backend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserMResponse {
     private String UserId;
-    private String massage;
+    private String message;
 }
